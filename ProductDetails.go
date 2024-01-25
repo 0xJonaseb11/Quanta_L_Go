2618ba5529c7f5c792ad99722a1bd979b38e65ad
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-25 15:32:08 +0200

    update product history

--- ProductDetails.go
+++ ProductDetails.go
@@ -18,3 +18,27 @@ type Product struct {
 	ManufactureDate uint64 `json:"manufactureDate"`
 	BatchNumber     string `json:"batchNumber"`
 }
+
+// ProductHistory represents the history of a product
+type ProductHistory struct {
+	Timestamp uint64        `json:"timestamp"`
+	Action    string        `json:"action"`
+	Location  string        `json:"location"`
+	State     ProductState `json:"state"`
+}
+
+// ProductState represents the state of a product
+type ProductState int
+
+const (
+	PRODUCT_REGISTERED ProductState = iota
+	QUALITY_ASSURANCE
+	PRODUCT_TRANSIT
+	PRODUCT_IN_INVENTORY
+	PRODUCT_SOLD
+	PRODUCT_RECALLED
+	CONSUMPTION
+	PENDING
+	VALIDATING
+	PUBLISHING
+)
